4260562c092279185cb553b590cbcee3970c3bff
Bernard Vander Beken <Bernard.VanderBeken@itineris.net>
2018-11-17 17:14:53 +0100

    Create NUnit3 test library. Add three slow tests with same behavior and different parameters.

---
+++ netfx-nunit/Tests/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestThreeSeconds()
+        {
+            SimulateSlowTest(3);
+        }
+
+        [Test]
+        public void TestFiveSeconds()
+        {
+            SimulateSlowTest(5);
+        }
+
+        [Test]
+        public void TestSevenSeconds()
+        {
+            SimulateSlowTest(7);
+        }
+
+        private static void SimulateSlowTest(int testDurationInSeconds)
+        {
+            int milliseconds = testDurationInSeconds * 1000;
+
+            // Simulate actual test work
+            Thread.Sleep(milliseconds);
+        }
+    }
+}
